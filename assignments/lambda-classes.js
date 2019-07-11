163d430302b0b3b1f11eb7abdacc5ca0bbdd932f
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-07-11 18:18:53 -0400

    working on the Lambda-classes file

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,23 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(attr) {
+    this.name = attr.name;
+    this.age = attr.age;
+    this.location = attr.location;
+    }
+    speak() {
+       return `Hello, my name is ${this.name}, I am from ${this.location}.`
+    }
+};
+
+class Instructor extends Person {
+    constructor(base) {
+        super(base);
+        this.specialty = base.specialty;
+        this.favLanguage = base.favLanguage;
+        this.catchPhrase = base.catchPhrase;
+    }
+    demo() {
+        
+    }
+}
